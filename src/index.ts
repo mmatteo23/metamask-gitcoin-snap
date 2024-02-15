99e3e115cfcfc45d6293de2091a63fd4e461e3b6
Matteo Midena <matteo.midena@protonmail.com>
2024-02-15 22:21:22 +0100

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -8,64 +8,16 @@ import type {
   StampOutput,
 } from './types';
 
-/**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
- *
- * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
- */
-// export const onRpcRequest: OnRpcRequestHandler = async ({
-//   origin,
-//   request,
-// }) => {
-//   const { method, params } = request;
-//   switch (method) {
-//     case 'hello':
-//       return snap.request({
-//         method: 'snap_dialog',
-//         params: {
-//           type: 'confirmation',
-//           content: panel([
-//             text(`Hello, **${origin}**!`),
-//             text('This custom confirmation is just for display purposes.'),
-//             text(
-//               'But you can edit the snap source code to make it do something, if you want to!',
-//             ),
-//           ]),
-//         },
-//       });
-//     case 'loadApiKey':
-//       isValidLoadApiKeyRequest(params as LoadApiKeyParams);
-//       await SnapStorage.save({
-//         apiKey: (params as LoadApiKeyParams).apiKey,
-//       });
-//       return snap.request({
-//         method: 'snap_dialog',
-//         params: {
-//           type: 'alert',
-//           content: panel([
-//             heading('API key loaded!'),
-//             text('You can now use the snap .'),
-//           ]),
-//         },
-//       });
-//     default:
-//       throw new Error('Method not found.');
-//   }
-// };
-
 export const onHomePage: OnHomePageHandler = async () => {
   return {
     content: panel([
       heading('gm mate!'),
+      text('Welcome to the Gitcoin Analytics Snap!'),
       text(
-        'Get insights about your transactions and be sure that you are sending funds to a human.',
+        'Enabling **KYR (Know Your Receiver) transactions**. Get stats on who you\'re sending money to directly on Metamask.',
       ),
-      text('If you have any issues, pls contact https://github.com/mmatteo23.'),
+      divider(),
+      text('If you have any issues, pls contact [https://github.com/mmatteo23](https://github.com/mmatteo23).'),
     ]),
   };
 };
